c14a22df4c9d18c697204af0dfc4317e5d281caf
RobertXing <rxing24@uwo.ca>
2025-11-19 02:53:19 -0500

    various test insertions

---
+++ SQL/ex3.sql
@@ -0,0 +1,36 @@
+INSERT INTO Salesperson (
+    SIN,
+    fName,
+    lName,
+    commission,
+    salary
+) VALUES (
+    12345,
+    'John',
+    'Doe',
+    0.05,
+    50000
+);
+
+INSERT INTO Salesperson
+SET
+    SIN = 67890,
+    fName = 'Jane',
+    lName = 'Doe',
+    commission = 0.05,
+    salary = 50000;
+
+INSERT INTO Manager (
+    SIN,
+    fName,
+    lName,
+    salary,
+    bonus
+) SELECT
+    SIN,
+    fName,
+    lName,
+    salary,
+    salary * 0.1
+FROM Salesperson
+WHERE SIN = 12345;
